c6e96fcee12069271f09fa9caba6538eebb5f603
Johan Roenby <jro@dhigroup.com>
2015-03-18 16:17:13 +0100

    Initial commit

---
+++ isoAdvector.C
@@ -0,0 +1,188 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2013 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    interFoam
+
+Description
+    Solver for 2 incompressible, isothermal immiscible fluids using a VOF
+    (volume of fluid) phase-fraction based interface capturing approach.
+
+    The momentum and other fluid properties are of the "mixture" and a single
+    momentum equation is solved.
+
+    Turbulence modelling is generic, i.e. laminar, RAS or LES may be selected.
+
+    For a two-fluid approach see twoPhaseEulerFoam.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "MULES.H"
+#include "subCycle.H"
+#include "interfaceProperties.H"
+#include "twoPhaseMixture.H"
+#include "turbulenceModel.H"
+//#include "pimpleControl.H"
+#include "fvIOoptionList.H"
+#include "volPointInterpolation.H"
+#include "isoCutter.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+//    pimpleControl pimple(mesh);
+
+    #include "initContinuityErrs.H"
+    #include "createFields.H"
+    #include "readTimeControls.H"
+//    #include "correctPhi.H"
+    #include "CourantNo.H"
+    #include "setInitialDeltaT.H"
+
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
+    {
+//        #include "readTimeControls.H"
+//        #include "CourantNo.H"
+//        #include "alphaCourantNo.H"
+//        #include "setDeltaT.H"
+
+        runTime++;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+		//Alpha loop
+		volPointInterpolation vpi(mesh);
+		alpha1.correctBoundaryConditions();
+		scalarField alphap = vpi.interpolate(alpha1);
+		
+		const scalar isoValue = 0.5;
+		isoCutter cutter(mesh,alphap,isoValue);
+		cutter.subFaceFractions(alphaf);
+		volScalarField dadt = fvc::surfaceIntegrate(phi*alphaf);
+//		Info << "min(|dV|) = " << min(mag(dadt)).value() << endl;
+//		dimensionedScalar invt("invt",  dimensionSet(0, 0, -1, 0, 0), SMALL);
+//		dimensionedScalar dt = min(mag((neg(dadt)*alpha1 + pos(dadt)*(1-alpha1))/(mag(dadt)-invt))
+//			+ (neg(alpha1-1e-6) + (pos(alpha1-1+1e-6)))/invt); //Does not work because setFields is used to initiate meaning that all cells are either completely full or completely empty.
+//		runTime.setDeltaT(max(dt.value(),1e-3));
+		alpha1 -= (runTime.deltaT()*dadt);
+		//Make list of overfilled cells
+		DynamicList<label> overFilledCells;
+		forAll(alpha1,ci)
+		{
+			if (alpha1[ci] > 1)
+			{
+				overFilledCells.append(ci);
+			}
+		}
+		overFilledCells.shrink();
+		label nOverFilledCells = overFilledCells.size();		
+		label ci = -1;
+		while (ci < nOverFilledCells-1)
+		{
+			ci++;
+			label cLabel = overFilledCells[ci];
+			Info << "Cell " << cLabel << ":" << endl;
+			labelList faceLabels = mesh.cells()[cLabel];
+			scalar magPhi = 0;
+			DynamicList<label> outFaces;
+			forAll(faceLabels,fi)
+			{
+				label fLabel = faceLabels[fi];
+				if (fLabel<mesh.nInternalFaces()) //face is internal and hence between two cells
+				{
+					if (cLabel == mesh.owner()[fLabel]) //positive flux means out of cell cLabel
+					{
+						if (phi[fLabel] > 0)
+						{
+							magPhi += phi[fLabel];
+							outFaces.append(fLabel);
+						}
+					}
+					else
+					{
+						if (phi[fLabel] < 0)
+						{
+							magPhi -= phi[fLabel];
+						}
+					}
+				}
+			}
+			outFaces.shrink();
+			forAll(outFaces,fi)
+			{
+				label fLabel = outFaces[fi];
+				label otherCellLabel;
+				if (cLabel == mesh.owner()[fLabel])
+				{
+					otherCellLabel = mesh.neighbour()[fLabel];
+				}
+				else
+				{
+					otherCellLabel = mesh.owner()[fLabel];					
+				}
+				scalar alphaOtherOld = alpha1[otherCellLabel];
+				scalar dV = (alpha1[cLabel]-1)*mesh.V()[cLabel]*mag(phi[fLabel])/magPhi;
+				Info << "Transferring " << dV << " m3 of water from cell " << cLabel << "to cell " << otherCellLabel << endl;
+				Info << "Old alpha1[cLabel]: " <<  alpha1[cLabel] << ", old alpha[otherCellLabel] = " << alpha1[otherCellLabel] << endl;
+				alpha1[otherCellLabel] += dV/mesh.V()[otherCellLabel];
+				alpha1[cLabel] -= dV/mesh.V()[cLabel];
+				Info << "New alpha1[cLabel]: " <<  alpha1[cLabel] << ", new alpha[otherCellLabel] = " << alpha1[otherCellLabel] << endl;
+				if (alphaOtherOld<1 && alpha1[otherCellLabel]>1)
+				{
+					overFilledCells.append(otherCellLabel);
+					nOverFilledCells++;
+					Info << "Cell " << otherCellLabel << " got overfilled!!!" << endl;
+				}
+			}
+		}
+		Info << "sum(alpha*V) = " << sum(mesh.V()*alpha1).value() 
+			 << ", max(alpha1) = " << max(alpha1).value() 
+			 << "\t min(alpha1) = " << min(alpha1).value() << endl;
+//			 << "\t min(dt) = " << dt.value() << endl; 
+
+        runTime.write();
+		cutter.write();
+		
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
